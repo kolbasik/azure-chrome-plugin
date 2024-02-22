12d17d5ec8c25310e095993158ca4003cfdd416f
kolbasik <kolbasik@users.noreply.github.com>
2024-02-22 10:18:13 +0100

    Improvements based on suggestions in issue-1 (#2)

--- extension/script.js
+++ extension/script.js
@@ -1,37 +1,55 @@
+const makePersistentProperty = (key, defaultValue, valueOf = (it) => it) =>
+  (value) => {
+    if (typeof value !== "undefined") {
+      return localStorage.setItem(key, value);
+    }
+    return valueOf(localStorage.getItem(key)) || defaultValue
+  };
+
+const MaxTimeoutInSeconds = makePersistentProperty("KOLBASIK__MAX_TIMEOUT", 30000, it => parseInt(it, 10));
+
 const getVisibleElements = (selector) =>
   [...document.querySelectorAll(selector)]
-  .filter(it => it.checkVisibility())
+    .filter((it) => it.checkVisibility());
 
-const loadValues = ({interval=250, timeout=30000} = {}) => {
-  return new Promise(resolve => {
-    let timer = null, stop = () => resolve(clearInterval(timer))
+const loadMore = () => {
+  const $loadMores = getVisibleElements(".azc-grid-pageable-loadMoreContainer");
 
-    timer = setInterval(() => {
-      var $loadMores = getVisibleElements(".azc-grid-pageable-loadMoreContainer")
+  for (let $loadMore of $loadMores) {
+    if (!$loadMore.classList.contains("fxs-display-none")) {
+      $loadMore?.click();
+      return true;
+    }
+  }
 
-      for (let $loadMore of $loadMores) {
-        if (!$loadMore.classList.contains("fxs-display-none")) {
-          $loadMore?.click()
-          return
-        }
-      }
+  return false;
+}
+const loadValues = ({ interval = 250, teardown = 500, timeoutInSeconds = MaxTimeoutInSeconds() } = {}) => {
+  return new Promise((resolve) => {
+    let timer = null;
 
-      stop();
-    }, interval)
+    let stop = () => {
+      clearInterval(timer);
+      setTimeout(resolve, teardown); // give a chance to load the last requested chuck.
+    };
 
-    setTimeout(stop, timeout)
+    timer = setInterval(() => loadMore() || stop(), interval);
+
+    setTimeout(stop, timeoutInSeconds * 1000);
   });
 };
 
 const getValueRows = () =>
-  getVisibleElements(".azc-grid-full").flatMap(it => [...it.querySelectorAll("tbody > tr")])
+  getVisibleElements(".azc-grid-full")
+    .flatMap((it) => [...it.querySelectorAll("tbody > tr")]);
 
-const filterValues = (term) => {
-  return getValueRows().filter((tr) => {
-    const matched = new RegExp(term, "gi").test(tr.innerText);
-    tr.style.display = matched ? "" : "none";
-    return matched;
-  });
+const filterValues = (keyword) => {
+  if (keyword) {
+    getValueRows().forEach((tr) => {
+      const matched = new RegExp(keyword, "gi").test(tr.innerText);
+      tr.style.display = matched ? "" : "none";
+    });
+  }
 };
 
 const resetFilter = () => {
@@ -40,35 +58,37 @@ const resetFilter = () => {
   });
 };
 
-const loadAndFilterValues = async (term) => {
-  await loadValues();
-  filterValues(term);
+const loadAndFilterValues = (keyword) => {
+  loadValues()
+    .then(() => filterValues(keyword));
 };
 
-const actions = {
-  "Q": function LoadAndFilterRows() {
-    const term = prompt("Term?");
-    term && loadAndFilterValues(term);
-  },
-  "L": function LoadRows() {
-    loadValues();
-  },
-  "F": function FilterRows() {
-    const term = prompt("Term?");
-    term && filterValues(term);
-  },
-  "Z": function ResetRows() {
-    resetFilter();
-  }
-}
+const help = ["Plugin is great to check KeyVault Secrets, Activity or Sign-in logs, or Blobs in a Storage Account Container, etc...\n"];
 
-document.body.addEventListener("keyup", ev => {
-  if (ev.ctrlKey) {
-    if (ev.key === '/') {
-      alert(Object.entries(actions).map(it => `Ctrl + Shift + ${it[0]} : ${it[1].name.replace(/([A-Z])/g, ' $1').trim()}`).join('\n'))
-    } else if (ev.shiftKey) {
-      const action = actions[ev.key]
-      action && action()
+  // 'CTRL+SHIFT+F - TITLE'
+const bindKey = (title, handler) => {
+  help.push(title);
+
+  const keys = title.split(" - ", 2).at(0).toUpperCase().split("+");
+  const options = {
+    key: keys.at(-1),
+    ctrlKey: keys.includes("CTRL"),
+    shiftKey: keys.includes("SHIFT")
+  };
+
+  document.addEventListener("keydown", event => {
+    if (event.ctrlKey === options.ctrlKey && event.shiftKey === options.shiftKey && event.key.toUpperCase() === options.key) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+      handler(event, options);
     }
-  }
-});
+  });
+};
+
+bindKey("Shift+T - Set MAX timeout in seconds.", () => MaxTimeoutInSeconds(prompt("Enter the MAX timeout in seconds:")));
+bindKey("Shift+N - Click on 'Load More' button once.", loadMore);
+bindKey("Shift+L - Keep 'Load More' until MAX timeout.", loadValues);
+bindKey("Shift+F - Filter by keyword.", () => filterValues(prompt("Enter the keyword to filter on:")));
+bindKey("Ctrl+Shift+F - Load more and filter by keyword.", () => loadAndFilterValues(prompt("Enter the keyword to load more and filter on:")));
+bindKey("Shift+Z - Reset the filter.", resetFilter);
+bindKey("Ctrl+/ - Help.", () => alert(help.join("\n")));
